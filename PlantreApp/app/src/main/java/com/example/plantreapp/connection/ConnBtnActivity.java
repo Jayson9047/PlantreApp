b3d79b4ffaeae9e7c2d3dff959ee8b2e43f28a14
Jayson9047 <56447811+Jayson9047@users.noreply.github.com>
2022-03-09 08:29:05 -0500

    Added Progress bar
    Progress bar code has been added

--- PlantreApp/app/src/main/java/com/example/plantreapp/connection/ConnBtnActivity.java
+++ PlantreApp/app/src/main/java/com/example/plantreapp/connection/ConnBtnActivity.java
@@ -24,15 +24,25 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 
+import android.os.Handler;
+import android.widget.ProgressBar;
+
+
 public class ConnBtnActivity extends AppCompatActivity {
 
     //Button ButtonConnectionPage;
 
     Button ButtonPump;
+    ProgressBar circular_pro;
+    Button clickme_btn;
+    TextView status;
+    private int progressStatus;
+    private Handler handler = new Handler();
+    private int soilMoisture = 0;
 
     private final static String TAG = MainActivity.class.getSimpleName();
 
-    TextView textViewPrompt;
+    //TextView textViewPrompt;
     boolean pumpOn;
     static final int UdpServerPORT = 4445;
     UdpServerThread udpServerThread;
@@ -41,7 +51,36 @@ public class ConnBtnActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conn_btn);
-        textViewPrompt = (TextView)findViewById(R.id.prompt);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNav);
+        bottomNavigationView.setSelectedItemId(R.id.home_item);
+
+        // nav click handler
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home_item:
+                        //startActivity(new Intent(getApplicationContext(), ConnBtnActivity.class));
+                        return true;
+                    case R.id.my_plants_item:
+                        startActivity(new Intent(getApplicationContext(), MyPlantsActivity.class));
+                        return true;
+                    case R.id.journals_item:
+                        startActivity(new Intent(getApplicationContext(), SearchActivity.class));
+                        return true;
+                    case R.id.connection_item:
+                        startActivity(new Intent(getApplicationContext(), ConnectionActivity.class));
+                        return true;
+                }
+                return false;
+            }
+        });
+
+        circular_pro = (ProgressBar)findViewById(R.id.progessbar_circular);
+        clickme_btn= (Button)findViewById(R.id.progess_btn);
+        status= (TextView)findViewById(R.id.text_status);
+        //textViewPrompt = (TextView)findViewById(R.id.prompt);
         pumpOn = false;
 
         ButtonPump = (Button) findViewById(R.id.btnSendPump);
@@ -50,6 +89,41 @@ public class ConnBtnActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 pumpOn = true;
+            }
+        });
+
+        clickme_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        while(true){
+                            handler.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    circular_pro.setProgress(soilMoisture);
+                                    if(soilMoisture <0 )
+                                    {
+                                        soilMoisture = 0;
+                                    }
+                                    if(soilMoisture > 100)
+                                    {
+                                        soilMoisture = 100;
+                                    }
+                                    status.setText(soilMoisture+"%");
+                                }
+                            });
+                            try {
+                                Thread.sleep(200);
+
+                            }catch (InterruptedException e){
+                                e.printStackTrace();
+                            }
+                        }
+
+                    }
+                }).start();
             }
         });
 
@@ -70,14 +144,14 @@ public class ConnBtnActivity extends AppCompatActivity {
         super.onStop();
     }
 
-    private void updatePrompt(final String prompt){
+    /*private void updatePrompt(final String prompt){
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
                 textViewPrompt.append(prompt);
             }
         });
-    }
+    }*/
 
     private class UdpServerThread extends Thread{
 
@@ -113,6 +187,21 @@ public class ConnBtnActivity extends AppCompatActivity {
 
                     String msg = new String(packet.getData(), packet.getOffset(), packet.getLength());
                     int l = Integer.valueOf(msg);
+                    circular_pro.setProgress(soilMoisture);
+
+                    if(soilMoisture <0 )
+                    {
+                        soilMoisture = 0;
+                    }
+                    else if(soilMoisture > 100)
+                    {
+                        soilMoisture = 100;
+                    }
+                    else
+                    {
+                        soilMoisture = l;
+                    }
+
 
                     //int li = ByteBuffer.wrap(packet.getData()).getInt();
 
@@ -122,7 +211,7 @@ public class ConnBtnActivity extends AppCompatActivity {
                     int port = packet.getPort();
 
                     //updatePrompt("Request from: " + address + ":" + port + "\n");
-                    updatePrompt("Message: "+ l +"\n");
+                    //updatePrompt("Message: "+ soilMoisture +"\n");
 
                     if(pumpOn == true)
                     {
