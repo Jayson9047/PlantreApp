9a1c543b305a5af5c1551d2e9a15eed22d0c19ed
Jayson9047 <56447811+Jayson9047@users.noreply.github.com>
2022-03-30 02:57:44 -0400

    updated esp32 code
    Updated hardware code

--- PlantreESP32/PlantreESP32.ino
+++ PlantreESP32/PlantreESP32.ino
@@ -1,3 +1,4 @@
+#include <pthread.h>
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
@@ -6,9 +7,11 @@
 #include <WiFiUdp.h>
 #include <String.h>
 #include "BluetoothSerial.h"
+#include <BlynkSimpleEsp32.h>
 
 
 #define WATERPUMP 5
+#define SECONDWATERPUMP 19
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino reset pin)
@@ -21,11 +24,21 @@
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 BluetoothSerial SerialBT; 
+//this is for first sensor
 const int AirValue = 2580;   //you need to replace this value with Value_1
 const int WaterValue = 1070;  //you need to replace this value with Value_2
-const int SensorPin = 36;
 int soilMoistureValue = 0;
 int soilmoisturepercent=0;
+const int SensorPin1 = 36;
+
+//this is for second sensor
+const int AirValue2 = 3380;   //you need to replace this value with Value_1
+const int WaterValue2 = 1370;  //you need to replace this value with Value_2
+const int SensorPin2 = 35;
+int soilMoistureValue2 = 0;
+int soilmoisturepercent2=0;
+char auth[] = "ihbYhRnEL8H3lw84v8fyU-CPtH-BJs00";
+
 bool wifiConnected = false;
 
 
@@ -44,52 +57,58 @@ const char * udpAddress;
 WiFiUDP udp;
 unsigned int udpPort;
 bool pumpOn;
+bool secondPumpOn;
 
 char pktbuf[10];
 String x_val;
 int randNum = 0;
+
+//--------------------------------------------------------------------------------------------------------------------------
+
  
 void setup() {
-  Serial.begin(115200); // open serial port, set the baud rate to 9600 bps
-  
-  SerialBT.begin("ESP32test"); //Bluetooth device name
-  Serial.println("The device started, now you can pair it with bluetooth!");
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); //initialize with the I2C addr 0x3C (128x64)
-  display.clearDisplay();
-  pinMode(WATERPUMP, OUTPUT);
-  digitalWrite(WATERPUMP, HIGH); 
-  pumpOn = false;
-  /*WiFi.begin(ssid,password);
-  while(WiFi.waitForConnectResult() != WL_CONNECTED)  {
-    Serial.println("Connection Failed! Rebooting...");
-    delay(5000);
-    ESP.restart();
-  }
-  Serial.println(WiFi.localIP());
-  Serial.println("Status: Connected");
-  udp.begin(udpPort);
-  Serial.println(udpPort);*/
-}
  
+   Serial.begin(115200);
+   SerialBT.begin("ESP32test"); //Bluetooth device name
+   Serial.println("The device started, now you can pair it with bluetooth!");
+   //display.begin(SSD1306_SWITCHCAPVCC, 0x3C); //initialize with the I2C addr 0x3C (128x64)
+   //display.clearDisplay();
+   pinMode(WATERPUMP, OUTPUT);
+   pinMode(SECONDWATERPUMP, OUTPUT);
+   digitalWrite(WATERPUMP, HIGH); 
+   digitalWrite(SECONDWATERPUMP, HIGH); 
+   pumpOn = false;
+   secondPumpOn = false;
+
+   display.begin(SSD1306_SWITCHCAPVCC, 0x3C); //initialize with the I2C addr 0x3C (128x64)
+   display.clearDisplay();
+   wifiConnected = false;
+   //Serial.println("Got it");
+   pthread_t threads[3];
+   int returnValue;
  
-void loop() 
-{
-  if(wifiConnected == false)
-  {
-      connectToWifi();
-  }
-  else
-  {      
-      readSoilMoistureValue();
-      sendUdpPacket();
-      showInOLED();
-      if(pumpOn == true)
+   for( int i = 0; i< 3; i++ ) {
+      if(i == 0)
       {
-        runWaterPump();
+        returnValue = pthread_create(&threads[i], NULL, printThreadId, (void *)i);
       }
-  }
-  
+      if(i == 1)
+      {
+        returnValue = pthread_create(&threads[i], NULL, printThreadId1, (void *)i);
+      }
+      if(i == 2)
+      {
+        returnValue = pthread_create(&threads[i], NULL, printThreadId2, (void *)i);
+      }
+      
+ 
+      if (returnValue) {
+         Serial.println("An error has occurred");
+      }
+   }
+ 
 }
+
 
 void connectToWifi()
 {
@@ -141,15 +160,38 @@ void connectToWifi()
         SerialBT.print("0");
         udp.begin(udpPort);
         Serial.println(udpPort);
+        Blynk.begin(auth,ssid,password);
       }
       delay(20);
+}
+ 
+void loop() 
+{
+   if(wifiConnected == false)
+   {
+      connectToWifi();
+   }
+   else
+   {
+      Blynk.run();
+   }
 }
 
 void readSoilMoistureValue()
 {
-    soilMoistureValue = analogRead(SensorPin);  //put Sensor insert into soil
-    Serial.println(soilMoistureValue);
+    soilMoistureValue = analogRead(SensorPin1);  //put Sensor insert into soil
     soilmoisturepercent = map(soilMoistureValue, AirValue, WaterValue, 0, 100);
+    Serial.println("pin1 percentage:");
+    Serial.println(soilmoisturepercent);
+    Serial.println("pin1 value:");
+    Serial.println(soilMoistureValue);
+    
+    soilMoistureValue2 = analogRead(SensorPin2);  //put Sensor insert into soil
+    soilmoisturepercent2 = map(soilMoistureValue2, AirValue2, WaterValue2, 0, 100);
+    Serial.println("pin2 percentage:");
+    Serial.println(soilmoisturepercent2);
+    Serial.println("pin2 value:");
+    Serial.println(soilMoistureValue2);
 }
 
 void sendUdpPacket()
@@ -157,7 +199,7 @@ void sendUdpPacket()
     int rp = udp.parsePacket();
     if(!rp)
     {
-        x_val = String(soilmoisturepercent);
+        x_val = (String)soilmoisturepercent + "," + soilmoisturepercent2;
         Serial.print("udp_send: ");
         Serial.println(x_val);
         udp.beginPacket(udpAddress, udpPort);
@@ -175,8 +217,77 @@ void sendUdpPacket()
       {
         pumpOn = true;
       }
+      else if(pktbuf[0] == '4')
+      {
+        secondPumpOn = true;
+      }
       delay(1000);
     }
+}
+
+void *printThreadId(void *threadid) {
+   
+   while(true)
+   {
+     
+     int i = 0;
+     if(pumpOn == true)
+     {
+       runWaterPump(WATERPUMP);
+     }
+     else
+     {
+       delay(1000);
+     }
+   }
+   return NULL;
+}
+
+void *printThreadId2(void *threadid) {
+   
+   while(true)
+   {    
+     int i = 0;
+     if(secondPumpOn == true)
+     {
+       runWaterPump(SECONDWATERPUMP);
+     }
+     else
+     {
+       delay(1000);
+     }
+   }
+   return NULL;
+}
+
+void *printThreadId1(void *threadid) {
+   
+   while(true)
+   {
+     int i = 0;
+     
+     if(wifiConnected == true)
+     {
+       readSoilMoistureValue();
+       sendUdpPacket();
+       showInOLED();
+     }
+     else
+     {
+       delay(1000);
+     }
+   }                     // wait for a second
+   return NULL;
+}
+
+void runWaterPump(int waterPumpPin)
+{
+  digitalWrite(waterPumpPin, LOW);   // turn the LED on (HIGH is the voltage level)
+  delay(10000);                       // wait for a second
+  digitalWrite(waterPumpPin, HIGH);    // turn the LED off by making the voltage LOW
+  
+  pumpOn = false;
+  secondPumpOn = false;
 }
 
 void showInOLED()
@@ -253,11 +364,14 @@ void showInOLED()
     }  
 }
 
-void runWaterPump()
+BLYNK_WRITE(V1)
 {
-  digitalWrite(WATERPUMP, LOW);   // turn the LED on (HIGH is the voltage level)
-  delay(10000);                       // wait for a second
-  digitalWrite(WATERPUMP, HIGH);    // turn the LED off by making the voltage LOW
-  delay(2000);                       // wait for a second
-  pumpOn = false;
+  int pinValue = param.asInt();
+  runWaterPump(WATERPUMP);
+}
+
+BLYNK_WRITE(V2)
+{
+  int pinValue = param.asInt();
+  runWaterPump(SECONDWATERPUMP);
 }
